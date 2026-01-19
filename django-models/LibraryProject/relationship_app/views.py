84c857a84284aa784240771f38eb3b1785b85aa2
ogbueze <ogbuezeokenyi@gmail.com>
2026-01-19 11:09:31 +0100

    debug

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ def index(request):
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
