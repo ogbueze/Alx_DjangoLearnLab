43683643fa495c8bfb26a19994ffb225c3c96a2b
ogbueze <ogbuezeokenyi@gmail.com>
2026-01-19 09:21:32 +0100

    debug

---
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -0,0 +1,66 @@
+import os
+import django
+
+# --------------------------------------------------
+# Django environment setup
+# --------------------------------------------------
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'library_project.settings'
+)
+django.setup()
+
+# --------------------------------------------------
+# Model imports
+# --------------------------------------------------
+from relationship_app.models import (
+    Author,
+    Book,
+    Library,
+    Librarian
+)
+
+# --------------------------------------------------
+# Queries
+# --------------------------------------------------
+
+def query_books_by_author(author_name):
+    """
+    Query all books by a specific author
+    """
+    books = Book.objects.filter(author__name=author_name)
+    return books
+
+
+def list_books_in_library(library_name):
+    """
+    List all books in a library
+    """
+    library = Library.objects.get(name=library_name)
+    books = library.book_set.all()  # or library.books.all() if related_name is set
+    return books
+
+
+def get_librarian_for_library(library_name):
+    """
+    Retrieve the librarian for a library
+    """
+    library = Library.objects.get(name=library_name)
+    return library.librarian
+
+
+# --------------------------------------------------
+# Optional test execution
+# --------------------------------------------------
+if __name__ == "__main__":
+    print("Books by Author:")
+    for book in query_books_by_author("Chinua Achebe"):
+        print(book.title)
+
+    print("\nBooks in Library:")
+    for book in list_books_in_library("Central Library"):
+        print(book.title)
+
+    print("\nLibrarian:")
+    librarian = get_librarian_for_library("Central Library")
+    print(librarian.name)
