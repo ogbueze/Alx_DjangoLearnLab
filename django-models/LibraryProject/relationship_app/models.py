0b42731a3162248e8f46f13be16680e0e9bf6896
ogbueze <ogbuezeokenyi@gmail.com>
2026-01-19 09:38:12 +0100

    remove space in the library model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -23,7 +23,7 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
-class Librarian (models.Model):
+class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
